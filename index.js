7f5a7e45089a9aac76e88e635c01bfbc4b20c1fa
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-19 09:29:59 -0600

    Initialize counter and update increment/decrement logic
    Refactor counter initialization and increment/decrement operations.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var counter = 0;
-var prefix = ++prefixIncrement;
-var postfix = postfixIncrement++;
-var prefix = --prefixDecrement;
-var postfix = postfixDecrement--;
+let counter = 100;
+let prefixIncrement = ++counter;
+let postfixIncrement = counter++;
+let prefixDecrement = --counter;
+let postfixDecrement = counter--;
 document.getElementById("result").innerHTML = counter;
+
+//should initialize a variable named counter to the number 100
+//should assign the prefix incremented value of counter to a variable named prefixIncrement
+//should assign the postfix incremented value of counter to a variable named postfixIncrement
+//should assign the prefix decrement value of counter to a variable named prefixDecrement
+//should assign the postfix decrement value of counter to a variable named postfixDecrement
+//should assign the innerHTML of the HTML element with the id result to the counter
